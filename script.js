043539c16584a74628cefad2b2b4f6519804bb6c
mohamed-ahmed-a-a <mahmhd0055@gmail.com>
2024-07-29 13:10:03 +0200

    Upload project files

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.getElementById('container');
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login');
+
+    if (container && registerBtn && loginBtn) {
+        registerBtn.addEventListener('click', () => {
+            container.classList.add("active");
+        });
+
+        loginBtn.addEventListener('click', () => {
+            container.classList.remove("active");
+        });
+    } else {
+        console.error('One or more elements are missing.');
+    }
+});
